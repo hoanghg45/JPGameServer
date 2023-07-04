bce21a66929ce7c0679d8e6b80343dfad3e64d24
Hoang.NNM <85442015+hoanghg45@users.noreply.github.com>
2023-07-04 10:11:05 +0700

    update

---
+++ Areas/Admin/Controllers/LoginAdminController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JPGame.Areas.Admin.Controllers
+{
+    public class LoginAdminController : Controller
+    {
+        readonly DBEntities db = new DBEntities();
+        // GET: Admin/Login
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+        public JsonResult CheckLogin(string username, string password)
+        {
+
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                return Json(
+             new
+             {
+                 status = "error",
+                 message = "Vui lòng nhập đầy đủ thông tin!"
+
+             }
+             , JsonRequestBehavior.AllowGet
+             );
+            }
+            if (!db.Users.Any(u => u.UserName.Trim().Equals(username)))
+            {
+                return Json(
+                new
+                {
+                    status = "error",
+                    message = "Tài khoản của bạn không tồn tại vui lòng kiểm tra lại!"
+
+                }
+                , JsonRequestBehavior.AllowGet
+                );
+
+            }
+            var user = db.Users.Where(u => u.UserName.Trim().Equals(username)).FirstOrDefault();
+            var checkpass = BCrypt.Net.BCrypt.Verify(password, user.Password);
+            if (!checkpass)
+            {
+                return Json(
+              new
+              {
+                  status = "error",
+                  message = "Mật khẩu không đúng vui lòng kiểm tra lại!"
+
+              }
+              , JsonRequestBehavior.AllowGet
+              );
+            }
+
+
+            return Json(
+              new
+              {
+                  status = "success",
+                  message = "Đang nhập thành công!"
+
+              }
+              , JsonRequestBehavior.AllowGet
+              );
+
+
+
+        }
+    }
+}
