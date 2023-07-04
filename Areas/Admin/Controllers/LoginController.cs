4a1922552482619fbcf5e280f333144cb2d76e7b
Hoang.NNM <85442015+hoanghg45@users.noreply.github.com>
2023-07-04 08:55:47 +0700

    FirstCommit

---
+++ Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JPGame.Areas.Admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        readonly DBEntities db = new DBEntities();
+        // GET: Admin/Login
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+        //public JsonResult CheckLogin()
+        //{
+
+        //}
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JPGame.MvcApplication" Language="C#" %>
