b9af42e4d411cf46e2b6bd0bed10438c8e86fda4
Hoang.NNM <85442015+hoanghg45@users.noreply.github.com>
2023-07-05 17:22:55 +0700

    Update

---
+++ Areas/assets/admin/layout.js
@@ -0,0 +1,22 @@
+﻿(function SetLayOut() {
+    SetAdminUser()
+
+})()
+function SetAdminUser() {
+    $.ajax({
+        type: "GET",
+        url: '/LoginAdmin/GetUserInfor',
+        datatype: "json",
+        success: function (data) {
+            if (data.status == "success") {
+                $('.Name').text(data.user.Name)
+            } else{
+                window.location.href = "/Admin/LoginAdmin"
+            }
+        },
+        error: function () {
+
+            window.location.href = "/Admin/LoginAdmin"
+        }
+    })
+}
